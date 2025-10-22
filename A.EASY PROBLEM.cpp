a7b127f168cd364aeb3d70026a17e5cb6fb902c2
Kazi Fabea Rahman <fabearahman@gmail.com>
2025-10-22 22:28:24 +0600

    Add files via upload

---
+++ A.EASY PROBLEM.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;  
+    cin >> t;  
+
+    while (t--) {
+        int n;
+        cin >> n;
+        int b=1;
+        int a = n - b;  
+
+        cout << a << endl;
+    }
+
+    return 0;
+}
