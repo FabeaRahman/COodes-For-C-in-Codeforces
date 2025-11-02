3fcfff228e0de53ce8ece0d650863f2f22a47614
Kazi Fabea Rahman <fabearahman@gmail.com>
2025-11-03 00:54:55 +0600

    Update A. CREATING A NEW STRING.cpp

---
+++ A. CREATING A NEW STRING.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string S, T;
+    
+    cin >> S;
+    cin >> T;
+
+    cout << S.length() << " " << T.length() << endl;
+    cout << S << " " << T << endl;
+
+    return 0;
+}
