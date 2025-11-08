244d02e7163083362a9251659438026c62d8ea4c
Kazi Fabea Rahman <fabearahman@gmail.com>
2025-11-08 23:26:29 +0600

    Update J. COUNT LETTERS.cpp

---
+++ J. COUNT LETTERS.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+
+    int freq[26] = {0};
+
+  
+    for (char ch : S) {
+        freq[ch - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0) {
+            cout << char('a' + i) << " : " << freq[i] << endl;
+        }
+    }
+
+    return 0;
+}
